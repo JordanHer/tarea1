62671a37943407a66ac8ace828c7acaa4a4f3390
jorda <jorda@LAPTOP-T3PFDD2H>
2022-09-27 21:32:57 -0600

    Primer commit de la tarea 1

---
+++ src/test/java/com/tarea1/Tarea1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
